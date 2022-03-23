e1589e110654cbffbf5801075b725b4508a28b6b
racheli-cohen <r4180213@gmail.com>
2022-03-23 16:37:46 +0200

    connect to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express=require("express")
-const product=require("./routers/product.router")
+const express = require("express")
+const product = require("./routers/product.router")
 
-const app=express()
-
+const app = express()
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/chineseAuction").then(() => {
+    console.log("connected to mogodb")
+}).catch(err => console.log(err));
 app.use(express.json)
 app.listen(4500, () => { console.log("waiting") })
