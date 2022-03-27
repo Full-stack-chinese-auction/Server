dd17b5d92aaba0c6ebe853c66eb1438a0e9b7cd7
racheliAdler <R.A0583259485@gmail.com>
2022-03-27 16:08:21 +0300

    productModel

---
+++ models/product.model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+    item_name: String,
+    item_description: String,
+    item_price: Number
+});
+const Product = mongoose.model("products",productSchema);
+module.exports={productSchema,Product}
