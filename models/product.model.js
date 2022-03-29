b9e641e764de48c74c23e3a5f07037fce1c62ae9
racheli-cohen <r4180213@gmail.com>
2022-03-29 16:37:47 +0300

    add src to item

--- models/product.model.js
+++ models/product.model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     item_name: String,
     item_description: String,
-    item_price: Number
+    item_price: Number,
+    item_src:String
 });
 const Product = mongoose.model("products",productSchema);
 module.exports={productSchema,Product}
